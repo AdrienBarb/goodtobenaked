c3792868b5dd85636e03907bd3872838de7c41a9
Adrien Barbier <adrien-barbier@hotmail.fr>
2024-07-24 21:22:33 +0200

    invoices

--- frontend/components/InvoiceTable.tsx
+++ frontend/components/InvoiceTable.tsx
@@ -1,20 +1,10 @@
 "use client";
 
 import React, { useState, useMemo, useRef, useEffect } from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import TableHead from "@mui/material/TableHead";
 import styles from "@/styles/SalesTable.module.scss";
-import NoResults from "@/components/Common/NoResults";
 import { useTranslations } from "next-intl";
 import useApi from "@/lib/hooks/useApi";
 import Text from "./Text";
-import clsx from "clsx";
-import TableRowSkeleton from "./LoadingSkeleton/TableRowSkeleton";
 import { Invoice } from "@/types/models/Invoice";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
@@ -95,124 +85,44 @@ const InvoiceTable = () => {
   }, [hasNextPage, fetchNextPage]);
 
   return (
-    <TableContainer
-      component={Paper}
-      sx={{ boxShadow: "none", backgroundColor: "#fff0eb" }}
-    >
-      <Table
-        sx={{ minWidth: 500 }}
-        size="small"
-        aria-label="custom pagination table"
-      >
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">
-              <Text weight="bolder">{t("incomes.invoice_number")}</Text>
-            </TableCell>
-            <TableCell sx={{}} align="left">
-              <Text weight="bolder">{t("incomes.invoice_status")}</Text>
-            </TableCell>
-            <TableCell sx={{}} align="left"></TableCell>
-          </TableRow>
-        </TableHead>
+    <div className={styles.container}>
+      {invoices.map((currentInvoice: Invoice, index) => {
+        return (
+          <div
+            className={styles.saleCard}
+            key={index}
+            style={{
+              backgroundColor: "#cecaff",
+            }}
+          >
+            <div>
+              <Text>{currentInvoice.title || currentInvoice._id}</Text>
+            </div>
+            <div className={styles.value}>
+              {currentInvoice.paid ? (
+                <div
+                  className={styles.commandButton}
+                  onClick={() => handleDownloadInvoices(currentInvoice?._id)}
+                >
+                  <FontAwesomeIcon icon={faDownload} color="white" />
+                </div>
+              ) : (
+                <Text customStyles={{ color: "white" }}>
+                  {t("incomes.waiting")}
+                </Text>
+              )}
+            </div>
+          </div>
+        );
+      })}
 
-        <TableBody>
-          {isFetching ? (
-            <>
-              <TableRow
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell
-                  colSpan={6}
-                  sx={{
-                    width: "100%",
-                  }}
-                >
-                  <TableRowSkeleton />
-                </TableCell>
-              </TableRow>
-              <TableRow
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell
-                  colSpan={6}
-                  sx={{
-                    width: "100%",
-                  }}
-                >
-                  <TableRowSkeleton />
-                </TableCell>
-              </TableRow>
-            </>
-          ) : invoices.length > 0 ? (
-            invoices.map((currentInvoice: Invoice, index: number) => (
-              <TableRow
-                key={index}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell
-                  scope="row"
-                  sx={{
-                    whiteSpace: "nowrap",
-                  }}
-                >
-                  <Text>{currentInvoice.title || currentInvoice._id}</Text>
-                </TableCell>
-
-                <TableCell
-                  scope="row"
-                  sx={{
-                    whiteSpace: "nowrap",
-                  }}
-                >
-                  {currentInvoice?.paid ? (
-                    <div className={clsx(styles.chip, styles.paid)}>
-                      {t("incomes.paid")}
-                    </div>
-                  ) : (
-                    <div className={clsx(styles.chip, styles.waiting)}>
-                      {t("incomes.waiting")}
-                    </div>
-                  )}
-                </TableCell>
-
-                <TableCell scope="row" align="right">
-                  {currentInvoice?.paid && (
-                    <div
-                      className={styles.commandButton}
-                      onClick={() =>
-                        handleDownloadInvoices(currentInvoice?._id)
-                      }
-                    >
-                      <FontAwesomeIcon icon={faDownload} />
-                    </div>
-                  )}
-                </TableCell>
-              </TableRow>
-            ))
-          ) : (
-            <TableRow
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell
-                colSpan={6}
-                sx={{
-                  width: "100%",
-                }}
-              >
-                <NoResults text={t("incomes.noInvoices")} />
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
       <div
         style={{ height: "10rem", display: "block", width: "100%" }}
         ref={loadMoreRef}
       >
         {isFetchingNextPage && <Loader />}
       </div>
-    </TableContainer>
+    </div>
   );
 };
 
