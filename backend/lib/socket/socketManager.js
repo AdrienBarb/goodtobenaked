be03132f129fb0233b26d00495eb35f3f4def270
Adrien Barbier <adrien-barbier@hotmail.fr>
2024-08-01 20:09:50 +0200

    add users in redis

--- backend/lib/socket/socketManager.js
+++ backend/lib/socket/socketManager.js
@@ -5,26 +5,27 @@ const { createAdapter } = require('@socket.io/redis-adapter');
 class SocketManager {
   constructor() {
     this.io = null;
-    this.users = [];
+    this.pubClient = null;
+    this.subClient = null;
   }
 
   init(httpServer) {
     console.log('process.env.REDIS_URL', process.env.REDIS_URL);
-    const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379'; // Utilisez l'URL de Redis depuis l'environnement
-    const pubClient = new Redis(redisUrl, {
+    const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
+    this.pubClient = new Redis(redisUrl, {
       retryStrategy(times) {
         const delay = Math.min(times * 50, 2000);
         return delay;
       },
       maxRetriesPerRequest: 20,
     });
-    const subClient = pubClient.duplicate();
+    this.subClient = this.pubClient.duplicate();
 
-    pubClient.on('error', (err) => {
+    this.pubClient.on('error', (err) => {
       console.error('Redis pubClient error:', err);
     });
 
-    subClient.on('error', (err) => {
+    this.subClient.on('error', (err) => {
       console.error('Redis subClient error:', err);
     });
 
@@ -34,7 +35,7 @@ class SocketManager {
         methods: ['GET', 'POST'],
         credentials: true,
       },
-      adapter: createAdapter(pubClient, subClient),
+      adapter: createAdapter(this.pubClient, this.subClient),
     });
 
     this.configureSocketEvents();
@@ -45,21 +46,23 @@ class SocketManager {
     this.io.on('connection', (socket) => {
       console.log('New client connected', socket.id);
 
-      socket.on('addUser', (userId) => {
+      socket.on('addUser', async (userId) => {
         console.log('User added:', userId);
-        this.addUser(userId, socket.id);
-        this.io.emit('getUsers', this.users);
+        await this.addUser(userId, socket.id);
+        const users = await this.getUsers();
+        this.io.emit('getUsers', users);
       });
 
-      socket.on('disconnect', () => {
+      socket.on('disconnect', async () => {
         console.log('Client disconnected', socket.id);
-        this.removeUser(socket.id);
-        this.io.emit('getUsers', this.users);
+        await this.removeUser(socket.id);
+        const users = await this.getUsers();
+        this.io.emit('getUsers', users);
       });
 
       // Send and get message
-      socket.on('sendMessage', ({ senderId, receiverId, message }) => {
-        const user = this.getUser(receiverId);
+      socket.on('sendMessage', async ({ senderId, receiverId, message }) => {
+        const user = await this.getUser(receiverId);
         if (user) {
           console.log('Message sent from', senderId, 'to', receiverId);
           this.io.to(user.socketId).emit('getMessage', {
@@ -70,8 +73,8 @@ class SocketManager {
       });
 
       // Send and get notification
-      socket.on('sendNotification', ({ receiverId, conversationId }) => {
-        const user = this.getUser(receiverId);
+      socket.on('sendNotification', async ({ receiverId, conversationId }) => {
+        const user = await this.getUser(receiverId);
         if (user) {
           console.log('Notification sent to', receiverId);
           this.io.to(user.socketId).emit('getNotification', {
@@ -82,26 +85,37 @@ class SocketManager {
     });
   }
 
-  addUser(userId, socketId) {
-    if (!this.users.some((user) => user.userId === userId)) {
-      this.users.push({ userId, socketId });
+  async addUser(userId, socketId) {
+    await this.pubClient.hset('users', userId, socketId);
+  }
+
+  async removeUser(socketId) {
+    const users = await this.pubClient.hgetall('users');
+    for (const userId in users) {
+      if (users[userId] === socketId) {
+        await this.pubClient.hdel('users', userId);
+      }
     }
   }
 
-  removeUser(socketId) {
-    this.users = this.users.filter((user) => user.socketId !== socketId);
+  async getUser(userId) {
+    const socketId = await this.pubClient.hget('users', userId);
+    return socketId ? { userId, socketId } : null;
   }
 
-  getUser(userId) {
-    return this.users.find((user) => user.userId === userId);
+  async getUsers() {
+    const users = await this.pubClient.hgetall('users');
+    return Object.keys(users).map((userId) => ({
+      userId,
+      socketId: users[userId],
+    }));
   }
 
-  emitToUser(userId, event, data) {
+  async emitToUser(userId, event, data) {
+    const users = await this.getUsers();
+    console.log('users ', users);
     console.log('user : ', userId);
-    console.log('this.users : ', this.users);
-
-    const user = this.getUser(userId);
-
+    const user = await this.getUser(userId);
     if (user) {
       console.log('Emitting event', event, 'to user', userId);
       this.io.to(user.socketId).emit(event, data);
